5c8e2910e41625a781f2b930bce15136d5948d1e
niki0211 <nicole.huber@i-med.ac.at>
2024-11-07 09:14:16 +0100

    marked Haplogroup Name as link

--- docs/js/haplogroups.js
+++ docs/js/haplogroups.js
@@ -49,7 +49,9 @@ document.addEventListener('DOMContentLoaded', function () {
             const row = document.createElement('tr');
             // show full HG-Sig when has mutation mode is active, normal HG-Sig otherwise
             row.innerHTML = `
-                <td style="white-space: nowrap">${node.data.name} &nbsp; ⓘ</td>
+                <td style="white-space: nowrap; color: blue; text-decoration: underline;">
+    ${node.data.name}
+</td>
                 <td>${searchModeToggle.checked ? formatHGSignature(hgMotifsData[node.data.name]) : formatHGSignature(node.data.HG)}</td>            `;
 
             // pointer change and tooltip
